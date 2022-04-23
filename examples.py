347e1268722b0bd5c21351a12fff4130757617b6
JoinChang <joinchang1206@gmail.com>
2022-04-23 19:08:04 +0800

    新增JPG表情：小天使

--- examples.py
+++ examples.py
@@ -1,8 +1,9 @@
 from main import ImageGenerator
+import sys
 
 # ceng, diu 需要两个图片
 #generator.generate("cuo", [open(r"D:\Documents\约稿\风神chino\2022头像.jpg", "rb"), open(r"D:\Documents\约稿\风神chino\2022头像.jpg", "rb")])
 
 generator = ImageGenerator()
-config = generator.search_config("喜报")
-generator.generate(config.id, ["寄"])
+config = generator.search_config("xts")
+print(generator.generate(config.id, ["Lxns", open(r"D:\Documents\约稿\风神chino\2022头像.jpg", "rb")]))
